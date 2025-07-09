a0cf65468952afca3c257532e180e82ffe663845
Aline Rosa <alinerosa_s@hotmail.com>
2025-07-09 20:19:37 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EstudaMais.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
